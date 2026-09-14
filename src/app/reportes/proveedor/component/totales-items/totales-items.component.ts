7dca29d83a3082c422271e887b8e41437d877f1e
Ferxxaa <gonzalezfernanda491gmail.com>
2026-09-14 11:41:28 -0300

    total proveedores evaluadas

--- src/app/reportes/proveedor/component/totales-items/totales-items.component.ts
+++ src/app/reportes/proveedor/component/totales-items/totales-items.component.ts
@@ -12,35 +12,24 @@ import { sProveedor } from '../../../../services/sProveedor.service';
 export class TotalesItemsComponent implements OnInit, OnChanges {
 
   @Input() reportEvalProv: ReportEvalProv[];
-  // reporteProveedor$: Observable<mReporteProveedor[]>;
   totalEval: number;
   ponderaciontotalEval: number;
+  totalProveedores: number;
 
-  constructor(
-    // private Proveedor: sProveedor
-  ) {
-    // this.reporteProveedor$ = this.Proveedor.getReporteProveedor(new Date().getFullYear());
-  }
+  constructor() { }
 
   ngOnInit() {
     this.totalOC()
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    // if (this.agno == 0)
-    //   this.reporteProveedor$ = this.Proveedor.getReporteProveedor(new Date().getFullYear())
-    // if (this.agno)
-    //   this.reporteProveedor$ = this.Proveedor.getReporteProveedor(this.agno);
     this.totalOC()
   }
 
   totalOC() {
-    // reporteProv.subscribe(res => {
-    //   this.totalEval = res.filter(el => el.OcEvaluadas && el.categoria != "0").reduce((acc, el) => acc + el.OcEvaluadas, 0)
-    //   this.ponderaciontotalEval = res.filter(el => el.OcEvaluadas && el.categoria != "0").reduce((acc, el) => acc + (el.evaluacion * el.OcEvaluadas / this.totalEval), 0)
-    // });
     this.totalEval = this.reportEvalProv.reduce((acc, el) => acc + el.cantidadEval, 0)
     this.ponderaciontotalEval = this.reportEvalProv.reduce((acc, el) => acc + (el.calificacion * el.cantidadEval / this.totalEval), 0)
+    this.totalProveedores = new Set(this.reportEvalProv.map(el => el.idProveedor)).size
   }
 
 }
